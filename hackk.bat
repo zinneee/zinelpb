7bf43d8004900cfb115e6b6da84bd7110d9f8209
zinneee <zinelabedinekhelifa@gmail.com>
2020-09-20 18:39:25 +0200

    Initial commit

---
+++ hackk.bat
@@ -0,0 +1,29 @@
+@echo off
+start avast.mp3
+
+start 1825.mp3
+color c
+echo si tu fermes la page ton pc va exploser
+echo attends juste hahahahahahaha
+echo si tu appuie sur une touche le virus va s activer tout seul
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+
+timeout 53
+start calc.bat
+cd..
+dir /s
